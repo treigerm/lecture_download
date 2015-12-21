51b619f3d0579562e9aa9fb37b21bcef371aee65
Tim Reichelt <treichelt6@gmail.com>
2015-12-21 11:40:20 +0100

    Add main and create folder

--- path_manager.py
+++ path_manager.py
@@ -7,10 +7,30 @@ def create_year():
     s1 = int(year) * 2 - 1
     s2 = int(year) * 2
 
+    # directory in which the files are created
+    dir_name = 'Desktop/UoE'
+
+    # change working directory to home directory
+    os.chdir(os.path.expanduser('~'))
+
     # create pathes for the semesters
-    path_s1 = os.path.join('Users', 'Tim', 'Desktop', 'UoE', 'Year %s' % year, 'Semester %s' % s1)
-    path_s2 = os.path.join('Users', 'Tim', 'Desktop', 'UoE', 'Year %s' % year, 'Semester %s' % s2)
+    path_s1 = os.path.join(dir_name, 'Year %s' % year, 'Semester %s' % s1)
+    path_s2 = os.path.join(dir_name, 'Year %s' % year, 'Semester %s' % s2)
 
     # create directories for semesters
-    os.makedirs(path_s1)
-    os.makedirs(path_s2)
+    create_folder(path_s1)
+    create_folder(path_s2)
+
+def create_folder(path):
+    print("Try to create %s" % path)
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print("Created %s" % path)
+    else:
+        print("%s already exists" % path)
+
+def main():
+    create_year()
+
+if __name__ == "__main__":
+    main()
