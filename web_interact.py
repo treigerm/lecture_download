9806d3da01b2db022a37bd90aa82d81591ba1fda
Tim Reichelt <treichelt6@gmail.com>
2015-12-22 11:44:38 +0100

    Create skeleton of new module web_interact

---
+++ web_interact.py
@@ -0,0 +1,19 @@
+import requests, webbrowser, bs4
+
+def find_website(c_name):
+    """Tries to find the course website of a given course."""
+    pass
+
+def find_lecs(link):
+    """Tries to find the lecture slides on a course web page."""
+    pass
+
+def download_lec(link, path):
+    """Downloads the pdf at the given link and saves it in the given path."""
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
