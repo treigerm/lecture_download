ffffb5035d96392d3d6e092d6ddfc40627479b58
Tim Reichelt <treichelt6@gmail.com>
2015-12-22 12:07:38 +0100

    Complete find_website.

--- web_interact.py
+++ web_interact.py
@@ -2,7 +2,27 @@ import requests, webbrowser, bs4
 
 def find_website(c_name):
     """Tries to find the course website of a given course."""
-    pass
+    # downloads the google result page
+    print("Google %s.." % c_name)
+    res = requests.get("http://google.com/search?q=" + c_name)
+    res.raise_for_status()
+
+    # retrieves top search result links
+    soup = bs4.BeautifulSoup(res.text, "lxml")
+
+    # gets the first result
+    first_result = soup.select('.r a')[0]
+    link = first_result.get('href')
+
+    # check if result is a university site
+    if "ed.ac.uk" in link:
+        print("Found a result.")
+        return "http://google.com" + link
+    else:
+        print("Did not find a course website for %s." % c_name)
+        return ""
+
+
 
 def find_lecs(link):
     """Tries to find the lecture slides on a course web page."""
@@ -13,7 +33,7 @@ def download_lec(link, path):
     pass
 
 def main():
-    pass
+    print(find_website("Informatics 1 - Data and Analysis"))
 
 if __name__ == "__main__":
     main()
